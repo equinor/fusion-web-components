7415b2d6cedc6b08ae7a1b036831f85711bd5bb6
Mats Oftedal <mats.oftedal@bouvet.no>
2021-06-07 12:36:17 +0200

    fix: removed duplicate css variable

--- packages/theme/src/style.ts
+++ packages/theme/src/style.ts
@@ -6,6 +6,5 @@ export const style = css`
     --mdc-theme-secondary: ${unsafeCSS(theme.colors.interactive.primary__resting.value.hex)};
     --mdc-theme-on-surface: ${unsafeCSS(theme.colors.text.static_icons__primary_white.value.hex)};
     --mdc-theme-surface: ${unsafeCSS(theme.colors.text.static_icons__secondary.value.hex)};
-    --mdc-theme-on-surface: ${unsafeCSS(theme.colors.text.static_icons__secondary.value.hex)};
   }
 `;

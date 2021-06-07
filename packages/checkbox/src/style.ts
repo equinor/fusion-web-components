c1855b3e358f850dd4af65bea1c04aba37cd2a97
Mats Oftedal <mats.oftedal@bouvet.no>
2021-06-07 12:11:00 +0200

    fix: added eds tokens

--- packages/checkbox/src/style.ts
+++ packages/checkbox/src/style.ts
@@ -1,9 +1,10 @@
-import { css } from 'lit-element';
+import { css, unsafeCSS } from 'lit-element';
+import { styles as theme } from '@equinor/fusion-web-theme';
 
 export const style = css`
   :host {
-    --mdc-checkbox-ink-color: var(--eds_text_static_icons__primary_white, rgba(255, 255, 255, 1));
-    --mdc-checkbox-unchecked-color: var(--eds_interactive_primary__resting, rgba(0, 112, 121, 1));
-    --mdc-checkbox-disabled-color: var(--eds_interactive_disabled__fill, rgba(234, 234, 234, 1));
+    --mdc-checkbox-ink-color: ${unsafeCSS(theme.colors.text.static_icons__primary_white.value.hex)};
+    --mdc-checkbox-unchecked-color: ${unsafeCSS(theme.colors.interactive.primary__resting.value.hex)};
+    --mdc-checkbox-disabled-color: ${unsafeCSS(theme.colors.interactive.disabled__fill.value.hex)};
   }
 `;

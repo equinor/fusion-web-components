75e463bca2cd43df7dd9aefdb30b6702c01a034b
Mats Oftedal <mats.oftedal@bouvet.no>
2021-06-07 14:23:14 +0200

    feat: added radio component

---
+++ packages/radio/src/index.ts
@@ -0,0 +1,17 @@
+import { CSSResult } from 'lit-element';
+import { fusionElement } from '@equinor/fusion-wc-core';
+import { RadioBase } from '@material/mwc-radio/mwc-radio-base';
+import { style as mdcStyle } from '@material/mwc-radio/mwc-radio-css';
+import { fusionTheme } from '@equinor/fusion-wc-theme';
+import { style } from './style';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'fwc-radio': Checkbox;
+  }
+}
+
+@fusionElement('fwc-radio')
+export default class Checkbox extends RadioBase {
+  static styles: CSSResult[] = [mdcStyle, fusionTheme, style];
+}

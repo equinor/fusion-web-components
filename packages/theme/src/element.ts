f3bab72d6d9d7a5da07701223b7ed6e20a18b586
Mats Oftedal <mats.oftedal@bouvet.no>
2021-06-16 15:02:59 +0200

    fix: added Equinor font to theme

--- packages/theme/src/element.ts
+++ packages/theme/src/element.ts
@@ -7,18 +7,19 @@ export default class Element extends LitElement {
   }
 
   render(): TemplateResult {
-    return html`<style>
-      :root {
-        --mdc-theme-primary: ${theme.colors.interactive.primary__resting.value.hex};
-        --mdc-theme-secondary: ${theme.colors.interactive.primary__resting.value.hex};
-        --mdc-theme-surface: ${theme.colors.ui.background__light.value.hex};
-        --mdc-theme-on-primary: ${theme.colors.text.static_icons__primary_white.value.hex};
-        --mdc-theme-on-secondary: ${theme.colors.text.static_icons__primary_white.value.hex};
-        --mdc-theme-on-surface: ${theme.colors.text.static_icons__secondary.value.hex};
-        --mdc-theme-background: #ffffff;
-        --mdc-ripple-color: ${theme.colors.interactive.primary__resting.value.hex};
-        --mdc-typography-font-family: Equinor;
-      }
-    </style>`;
+    return html` <link href="https://eds-static.equinor.com/font/equinor-font.css" rel="stylesheet" />
+      <style>
+        :root {
+          --mdc-theme-primary: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-theme-secondary: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-theme-surface: ${theme.colors.ui.background__light.value.hex};
+          --mdc-theme-on-primary: ${theme.colors.text.static_icons__primary_white.value.hex};
+          --mdc-theme-on-secondary: ${theme.colors.text.static_icons__primary_white.value.hex};
+          --mdc-theme-on-surface: ${theme.colors.text.static_icons__secondary.value.hex};
+          --mdc-theme-background: #ffffff;
+          --mdc-ripple-color: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-typography-font-family: Equinor;
+        }
+      </style>`;
   }
 }

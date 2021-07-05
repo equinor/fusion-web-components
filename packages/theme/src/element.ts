d543b87224db4609695e20c11d79363ccf8d4b14
Mats Oftedal <mats.oftedal@bouvet.no>
2021-07-05 12:41:02 +0200

    fix: fixed colors and added icon font to theme

--- packages/theme/src/element.ts
+++ packages/theme/src/element.ts
@@ -8,16 +8,18 @@ export default class Element extends LitElement {
 
   render(): TemplateResult {
     return html` <link href="https://eds-static.equinor.com/font/equinor-font.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       <style>
         :root {
-          --mdc-theme-primary: ${theme.colors.interactive.primary__resting.value.hex};
-          --mdc-theme-secondary: ${theme.colors.interactive.primary__resting.value.hex};
-          --mdc-theme-surface: ${theme.colors.ui.background__light.value.hex};
+          --mdc-ripple-color: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-theme-background: #ffffff;
+          --mdc-theme-error: ${theme.colors.interactive.danger__text.value.hex};
           --mdc-theme-on-primary: ${theme.colors.text.static_icons__primary_white.value.hex};
           --mdc-theme-on-secondary: ${theme.colors.text.static_icons__primary_white.value.hex};
           --mdc-theme-on-surface: ${theme.colors.text.static_icons__secondary.value.hex};
-          --mdc-theme-background: #ffffff;
-          --mdc-ripple-color: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-theme-primary: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-theme-secondary: ${theme.colors.interactive.primary__resting.value.hex};
+          --mdc-theme-surface: ${theme.colors.ui.background__light.value.hex};
           --mdc-typography-font-family: Equinor;
         }
       </style>`;

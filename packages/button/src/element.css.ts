eb5492a473bf277866a8dc44827e280b4918790e
Mats Oftedal <mats.oftedal@bouvet.no>
2021-07-15 15:40:20 +0200

    feat: add new button component

--- packages/button/src/element.css.ts
+++ packages/button/src/element.css.ts
@@ -3,10 +3,46 @@ import { styles as theme } from '@equinor/fusion-web-theme';
 
 export const style = css`
   :host {
-    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.primary__resting.value.hex)};
-    --mdc-button-disabled-fill-color: ${unsafeCSS(theme.colors.interactive.disabled__fill.value.hex)};
-    --mdc-button-disabled-ink-color: ${unsafeCSS(theme.colors.interactive.disabled__text.value.hex)};
-    --mdc-button-disabled-outline-color: ${unsafeCSS(theme.colors.interactive.disabled__border.value.hex)};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    --mdc-button-disabled-fill-color: ${unsafeCSS(theme.colors.interactive.disabled__fill.getVariable('color'))};
+    --mdc-button-disabled-ink-color: ${unsafeCSS(theme.colors.interactive.disabled__text.getVariable('color'))};
+    --mdc-button-disabled-outline-color: ${unsafeCSS(theme.colors.interactive.disabled__border.getVariable('color'))};
+    --mdc-typography-button-text-transform: none;
+  }
+  .leading-icon fwc-icon,
+  .trailing-icon fwc-icon,
+  ::slotted(fwc-icon) {
+    font-size: 20px;
+  }
+  .mdc-button.mdc-button--unelevated.fwc-button--primary:not(:disabled) {
+    background-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+  }
+  .mdc-button.mdc-button--unelevated.fwc-button--secondary:not(:disabled) {
+    background-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+  }
+  .mdc-button.mdc-button--unelevated.fwc-button--danger:not(:disabled) {
+    background-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+  }
+  .mdc-button.mdc-button--outlined.fwc-button--primary:not(:disabled) {
+    border-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+  }
+  .mdc-button.mdc-button--outlined.fwc-button--secondary:not(:disabled) {
+    border-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+    color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+  }
+  .mdc-button.mdc-button--outlined.fwc-button--danger:not(:disabled) {
+    border-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+  }
+  .mdc-button.fwc-button--ghost.fwc-button--primary:not(:disabled) {
+    color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+  }
+  .mdc-button.fwc-button--ghost.fwc-button--secondary:not(:disabled) {
+    color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+  }
+  .mdc-button.fwc-button--ghost.fwc-button--danger:not(:disabled) {
+    color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
   }
 `;
 
--- packages/button/src/element.ts
+++ packages/button/src/element.ts
@@ -1,16 +1,46 @@
 import { html, property, CSSResult, TemplateResult } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
 import { IconName } from '@equinor/fusion-wc-icon';
 import { ButtonBase } from '@material/mwc-button/mwc-button-base';
 import { style as mdcStyle } from '@material/mwc-button/styles-css';
 import style from './element.css';
 
-export class ButtonElement extends ButtonBase {
+export type ButtonColor = 'primary' | 'secondary' | 'danger';
+
+export type ButtonVariant = 'contained' | 'outlined' | 'ghost';
+
+export interface ButtonProps {
+  icon?: IconName;
+  variant?: ButtonVariant;
+  color?: ButtonColor;
+}
+
+export class ButtonElement extends ButtonBase implements ButtonProps {
   static styles: CSSResult[] = [mdcStyle, style];
 
   @property() icon: IconName = '';
+  @property() color: ButtonColor = 'primary';
+  @property() variant: ButtonVariant = 'contained';
+  @property({ type: Boolean, reflect: true }) unelevated: boolean = false;
+
+  constructor() {
+    super();
+  }
+
+  protected getRenderClasses() {
+    return classMap({
+      'mdc-button--unelevated': this.variant === 'contained',
+      'mdc-button--outlined': this.variant === 'outlined',
+      'fwc-button--ghost': this.variant === 'ghost',
+      'mdc-button--dense': this.dense,
+      'fwc-button--primary': this.color === 'primary',
+      'fwc-button--secondary': this.color === 'secondary',
+      'fwc-button--danger': this.color === 'danger',
+    });
+  }
 
   protected renderIcon(): TemplateResult {
-    return html`<fwc-icon icon=${this.icon}></fwc-icon>`;
+    return html`<fwc-icon class="mdc-button__icon" icon=${this.icon}></fwc-icon>`;
   }
 }
 

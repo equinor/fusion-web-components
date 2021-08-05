5fcb48bd0c0a7192d8d9765b9e42b8de39bd5e9e
Mats Oftedal <mats.oftedal@bouvet.no>
2021-08-05 12:55:45 +0200

    fix: added better support for formats and fixed readme

--- packages/date/README.md
+++ packages/date/README.md
@@ -8,4 +8,34 @@
 npm install @equinor/fusion-wc-date
 ```
 
+## Example Usage
+
+```ts
+const date = new Date();
+return <fwc-date variant='distance' date={date}/>
+```
+
+### Properties/Attributes
+
+Name                    | Type             | Default      | Description
+---------------------   | --------------   | -----------  | -----------------
+`date`                  | `Date`           | `new Date()` | The date to format.
+`baseDate`              | `Date`           | `new Date()` | The date to compare with for variants `distance` and `relative`.
+`variant`               | `DateVariant*`   | `datetime`   | Templated variant to use as basis for format calculation, defaults to `datetime` (`HH:mm dd.MM.yyyy`).
+`format`                | `string`         | ''           | Custom format to use instead of templated variants. Overrides `variant` property to `custom` if assigned. Based on [Unicode Technical Standard #35](https://www.unicode.org/reports/tr35/tr35-dates.html#Date_Field_Symbol_Table).
+`locale`                | `Locale`         | `enGB`       | Used to override the locale for which the date format calculation is based. See date-fns [`Locale`](https://date-fns.org/v2.23.0/docs/Locale).
+`capitalizeFirstLetter` | `boolean`        | `false`      | Set to `true` to capitalize the first letter in the formatted string.
+`includeSeconds`        | `boolean`        | `false`      | Set to `true` to include seconds in the date format. Applies to variants `datetime`, `time` and `distance`.
+`weekStartsOn`          | `WeekDay**`      | `1`          | The index of the first day of the week (`0 = Sunday`).
+`addSuffix`             | `boolean`        | `false`      | Include a suffix to indicate if the `date` is before or after the `baseDate`. Only applies to variant `distance`.
+
+\*  `DateVariant` is exported by `fwc-date`.
+```ts
+type DateVariant = 'date' | 'time' | 'datetime' | 'relative' | 'distance' | 'custom';
+```
+
+\*\*  `WeekDay` is exported by `fwc-date`.
+```ts
+type WeekDay = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+```
 <!--prettier-ignore-end-->
--- packages/date/src/element.ts
+++ packages/date/src/element.ts
@@ -1,34 +1,47 @@
-import { html, LitElement, property, TemplateResult } from 'lit-element';
+import { html, LitElement, property, PropertyValues, TemplateResult } from 'lit-element';
 import { format, formatDistance, formatRelative, Locale } from 'date-fns';
 import { enGB } from 'date-fns/locale';
 
 const defaultDateFormat = 'dd.MM.yyyy';
 const defaultTimeFormat = 'HH:mm';
+const defaultTimeSecondsFormat = 'HH:mm:ss';
 const defaultDateTimeFormat = 'HH:mm dd.MM.yyyy';
+const defaultDateTimeSecondsFormat = 'HH:mm:ss dd.MM.yyyy';
 
-export type DateVariant = 'date' | 'time' | 'datetime' | 'relative' | 'distance';
+export type DateVariant = 'date' | 'time' | 'datetime' | 'relative' | 'distance' | 'custom';
+
+export type WeekDay = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
 export interface DateElementProps {
+  addSuffix?: boolean;
+  capitalizeFirstLetter?: boolean;
   date?: Date;
-  dateFrom?: Date;
-  dateTo?: Date;
+  baseDate?: Date;
   format?: string;
+  includeSeconds?: boolean;
   locale?: Locale;
   variant?: DateVariant;
+  weekStartsOn?: WeekDay;
 }
 
 export class DateElement extends LitElement implements DateElementProps {
+  @property({ type: Boolean, reflect: true })
+  addSuffix: boolean = false;
+
+  @property({ type: Boolean, reflect: true })
+  capitalizeFirstLetter: boolean = false;
+
   @property({ type: Object, reflect: true })
   date: Date = new Date();
 
   @property({ type: Object, reflect: true })
-  dateFrom?: Date = undefined;
-
-  @property({ type: Object, reflect: true })
-  dateTo?: Date = undefined;
+  baseDate: Date = new Date();
 
   @property({ type: String, reflect: true })
   format?: string = undefined;
+
+  @property({ type: Boolean, reflect: true })
+  includeSeconds?: boolean = false;
 
   @property({ type: Object, reflect: true })
   locale: Locale = enGB;
@@ -39,9 +52,12 @@ export class DateElement extends LitElement implements DateElementProps {
   @property({ type: String, reflect: true })
   variant: DateVariant = 'datetime';
 
+  @property({ type: Number, reflect: true })
+  weekStartsOn: WeekDay = 1;
+
   constructor() {
     super();
-    this.dateFrom = this.date;
+    this.baseDate = this.date;
   }
 
   protected resolveDateString(): string {
@@ -53,20 +69,44 @@ export class DateElement extends LitElement implements DateElementProps {
       case 'date':
         return format(this.date, defaultDateFormat, { locale: this.locale });
       case 'time':
-        return format(this.date, defaultTimeFormat, { locale: this.locale });
+        return format(this.date, this.includeSeconds ? defaultTimeSecondsFormat : defaultTimeFormat, {
+          locale: this.locale,
+        });
       case 'datetime':
-        return format(this.date, defaultDateTimeFormat, { locale: this.locale });
+        return format(this.date, this.includeSeconds ? defaultDateTimeSecondsFormat : defaultDateTimeFormat, {
+          locale: this.locale,
+        });
       case 'relative':
-        return formatRelative(this.dateFrom ?? new Date(), this.dateTo ?? new Date(), { locale: this.locale });
+        return formatRelative(this.date, this.baseDate, { locale: this.locale });
       case 'distance':
-        return formatDistance(this.dateFrom ?? new Date(), this.dateTo ?? new Date(), { locale: this.locale });
+        return formatDistance(this.date, this.baseDate, {
+          locale: this.locale,
+          includeSeconds: this.includeSeconds,
+          addSuffix: this.addSuffix,
+        });
       default:
-        return format(this.date ?? new Date(), defaultDateTimeFormat, { locale: this.locale });
+        return format(this.date, defaultDateTimeFormat, { locale: this.locale });
+    }
+  }
+
+  protected updated(changedProperties: PropertyValues) {
+    super.updated(changedProperties);
+    if (changedProperties.has('format')) {
+      if (this.format) {
+        this.variant = 'custom';
+      }
+      this.requestUpdate();
     }
   }
 
   protected render(): TemplateResult {
-    return html`<span>${this.resolveDateString()}</span>`;
+    let dateString = this.resolveDateString();
+
+    if (this.capitalizeFirstLetter) {
+      dateString = dateString.charAt(0).toUpperCase() + dateString.slice(1);
+    }
+
+    return html`<span>${dateString}</span>`;
   }
 }
 

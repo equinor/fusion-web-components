0ec0acb16e9feda08e2025723e1f850f52458c03
tobiasny <tobnyg@hotmail.no>
2021-09-08 10:20:18 +0200

    fix icon wc props

--- packages/icon/src/element.ts
+++ packages/icon/src/element.ts
@@ -1,11 +1,11 @@
-import { IconName } from '@equinor/eds-icons';
+import { IconData } from '@equinor/eds-icons';
 import { LitElement, property, SVGTemplateResult } from 'lit-element';
 import createIcon, { IconType } from './utils/create-icon';
 
 import { style } from './element.css';
 
 export type IconElementProps = {
-  icon: IconName;
+  icon: IconData;
   type?: IconType;
 };
 
@@ -13,12 +13,12 @@ export class IconElement extends LitElement implements IconElementProps {
   static styles = [style];
 
   @property()
-  public icon!: IconName;
+  public icon!: IconData;
 
   @property()
   public type?: IconType;
 
   render(): SVGTemplateResult {
-    return createIcon(this.icon);
+    return createIcon(this.icon.name);
   }
 }

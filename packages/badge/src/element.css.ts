9970e4bcf78c416f68001ad0b3c35a1b5db59293
Mats Oftedal <mats.oftedal@bouvet.no>
2021-09-16 14:28:41 +0200

    fix: fixed circular offset for sizes, added disabled and fixed hover

--- packages/badge/src/element.css.ts
+++ packages/badge/src/element.css.ts
@@ -3,6 +3,14 @@ import { styles as theme } from '@equinor/fusion-web-theme';
 
 export const style = css`
   :host {
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    --fwc-badge-size: 1.5rem;
+    --fwc-badge-font-size: 0.75rem;
+    background-color: var(--fwc-badge-color);
+    min-width: var(--fwc-badge-size);
+    height: var(--fwc-badge-size);
+    font-size: var(--fwc-badge-font-size);
+    border-radius: var(--fwc-badge-font-size);
     position: absolute;
     box-sizing: border-box;
     display: flex;
@@ -26,114 +34,104 @@ export const style = css`
     padding: 0 0.5rem;
   }
   :host([size='x-small']) {
-    min-width: 0.75rem;
-    height: 0.75rem;
-    font-size: 0.375rem;
-    border-radius: 0.375rem;
+    --fwc-badge-size: 0.75rem;
+    --fwc-badge-font-size: 0.375rem;
   }
   :host([size='small']) {
-    min-width: 1rem;
-    height: 1rem;
-    font-size: 0.5rem;
-    border-radius: 0.5rem;
+    --fwc-badge-size: 1rem;
+    --fwc-badge-font-size: 0.5rem;
   }
   :host([size='medium']) {
-    min-width: 1.5rem;
-    height: 1.5rem;
-    font-size: 0.75rem;
-    border-radius: 0.75rem;
+    --fwc-badge-size: 1.5rem;
+    --fwc-badge-font-size: 0.75rem;
   }
   :host([size='large']) {
-    min-width: 2rem;
-    height: 2rem;
-    font-size: 1rem;
-    border-radius: 1rem;
-  }
-  :host([position='top-left']:not([circular])) {
-    transform: scale(1) translate(-50%, -50%);
-  }
-  :host([position='top-left'][size='x-small'][circular]) {
-    transform: scale(1) translate(15%, 15%);
-  }
-  :host([position='top-left'][size='small'][circular]) {
-    transform: scale(1) translate(0, 0);
-  }
-  :host([position='top-left'][circular]) {
-    transform: scale(1) translate(-15%, -15%);
-  }
-  :host([position='top-right']:not([circular])) {
-    transform: scale(1) translate(50%, -50%);
-  }
-  :host([position='top-right'][size='x-small'][circular]) {
-    transform: scale(1) translate(-15%, 15%);
-  }
-  :host([position='top-right'][size='small'][circular]) {
-    transform: scale(1) translate(0, 0);
-  }
-  :host([position='top-right'][circular]) {
-    transform: scale(1) translate(15%, -15%);
-  }
-  :host([position='bottom-left']:not([circular])) {
-    transform: scale(1) translate(-50%, 50%);
-  }
-  :host([position='bottom-left'][size='x-small'][circular]) {
-    transform: scale(1) translate(15%, -15%);
-  }
-  :host([position='bottom-left'][size='small'][circular]) {
-    transform: scale(1) translate(0, 0);
-  }
-  :host([position='bottom-left'][circular]) {
-    transform: scale(1) translate(-15%, 15%);
-  }
-  :host([position='bottom-right']:not([circular])) {
-    transform: scale(1) translate(50%, 50%);
-  }
-  :host([position='bottom-right'][circular]) {
-    transform: scale(1) translate(15%, 15%);
-  }
-  :host([position='bottom-right'][size='x-small'][circular]) {
-    transform: scale(1) translate(-15%, -15%);
-  }
-  :host([position='bottom-right'][size='small'][circular]) {
-    transform: scale(1) translate(0, 0);
+    --fwc-badge-size: 2rem;
+    --fwc-badge-font-size: 1rem;
   }
   :host([position='top-left']) {
-    top: 0;
-    left: 0;
+    transform: scale(1) translate(-50%, -50%);
     transform-origin: 0% 0%;
   }
+  :host([position='top-left']:not([circular])) {
+    top: 0;
+    left: 0;
+  }
+  :host([position='top-left'][circular]) {
+    top: 15%;
+    left: 15%;
+  }
   :host([position='top-right']) {
+    transform: scale(1) translate(50%, -50%);
+    transform-origin: 0% 100%;
+  }
+  :host([position='top-right']:not([circular])) {
     top: 0;
     right: 0;
-    transform-origin: 0% 100%;
+  }
+  :host([position='top-right'][circular]) {
+    top: 15%;
+    right: 15%;
   }
   :host([position='bottom-left']) {
-    bottom: 0;
-    left: 0;
+    transform: scale(1) translate(-50%, 50%);
     transform-origin: 0% 100%;
   }
-  :host([position='bottom-right']) {
+  :host([position='bottom-left']:not([circular])) {
     bottom: 0;
-    right: 0;
+    left: 0;
+  }
+  :host([position='bottom-left'][circular]) {
+    bottom: 15%;
+    left: 15%;
+  }
+  :host([position='bottom-right']) {
+    transform: scale(1) translate(50%, 50%);
     transform-origin: 100% 100%;
   }
+  :host([position='bottom-right']:not([circular])) {
+    bottom: 0;
+    right: 0;
+  }
+  :host([position='bottom-right'][circular]) {
+    bottom: 15%;
+    right: 15%;
+  }
   :host([color='primary']) {
-    background-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+  }
+  :host([color='primary'][clickable]:hover) {
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.primary__hover.getVariable('color'))};
   }
   :host([color='secondary']) {
-    background-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+  }
+  :host([color='secondary'][clickable]:hover) {
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.secondary__link_hover.getVariable('color'))};
   }
   :host([color='success']) {
-    background-color: ${unsafeCSS(theme.colors.interactive.success__resting.getVariable('color'))};
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.success__resting.getVariable('color'))};
+  }
+  :host([color='success'][clickable]:hover) {
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.success__hover.getVariable('color'))};
   }
   :host([color='danger']) {
-    background-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+  }
+  :host([color='danger'][clickable]:hover) {
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.danger__hover.getVariable('color'))};
   }
   :host([color='warning']) {
-    background-color: ${unsafeCSS(theme.colors.interactive.warning__resting.getVariable('color'))};
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.warning__resting.getVariable('color'))};
+  }
+  :host([color='warning'][clickable]:hover) {
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.warning__hover.getVariable('color'))};
   }
   :host([color='disabled']) {
-    background-color: ${unsafeCSS(theme.colors.interactive.disabled__text.getVariable('color'))};
+    --fwc-badge-color: ${unsafeCSS(theme.colors.interactive.disabled__text.getVariable('color'))};
+  }
+  :host([clickable]:not([color='disabled']):hover) {
+    cursor: pointer;
   }
 `;
 

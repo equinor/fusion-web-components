8060c4cbaf890d6aec05cfeb932e61c479736cd3
odin thomas rochmann <odin.rochmann@gmail.com>
2021-09-27 00:53:02 +0200

    fix(badge): reflect required attributes for styling

--- packages/badge/src/element.ts
+++ packages/badge/src/element.ts
@@ -1,4 +1,4 @@
-import { LitElement, CSSResult, TemplateResult, PropertyValues, html } from 'lit';
+import { LitElement, TemplateResult, PropertyValues, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { IconName } from '@equinor/fusion-wc-icon';
@@ -8,45 +8,40 @@ export type BadgeSize = 'x-small' | 'small' | 'medium' | 'large';
 export type BadgePosition = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
 export type BadgeColor = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'disabled';
 
-export type BadgeElementProps = {
-  size?: BadgeSize;
-  position?: BadgePosition;
-  color?: BadgeColor;
-  value?: string;
-  icon?: IconName;
-  circular?: boolean;
-  tooltip?: string;
-  clickable?: boolean;
-  disabled?: boolean;
-};
+export class BadgeElement extends LitElement {
+  /** Size of the badge */
+  @property({ type: String, reflect: true })
+  size: BadgeSize = 'medium';
 
-export class BadgeElement extends LitElement implements BadgeElementProps {
-  static styles: CSSResult[] = [style];
-
-  @property({ type: String })
-  size?: BadgeSize;
-
+  /** Absolute corner position for the badge */
   @property({ type: String, reflect: true })
   position: BadgePosition = 'top-right';
 
+  /** Color of the badge */
   @property({ type: String, reflect: true })
-  color?: BadgeColor;
+  color: BadgeColor = 'secondary';
 
+  /** Text value to be rendered within the badge */
   @property({ type: String })
   value?: string;
 
-  @property({ type: String })
+  /** Icon to be rendered within the badge */
+  @property({ type: String, reflect: true })
   icon?: IconName;
 
-  @property({ type: Boolean })
+  /** Set to `true` if badge is placed within a circular wrapper for correct position */
+  @property({ type: Boolean, reflect: true })
   circular?: boolean;
 
+  /** Tooltip text to show on hover */
   @property({ type: String })
   tooltip?: string;
 
+  /** If the badge is clickable */
   @property({ type: Boolean, reflect: true })
   clickable?: boolean;
 
+  /** If the badge is disabled */
   @property({ type: Boolean })
   disabled?: boolean;
 
@@ -75,5 +70,7 @@ export class BadgeElement extends LitElement implements BadgeElementProps {
     return html`<span><slot></slot></span>`;
   }
 }
+
+BadgeElement.styles = [style];
 
 export default BadgeElement;

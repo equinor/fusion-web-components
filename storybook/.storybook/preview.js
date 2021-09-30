ed1bf5539c1d1248f63fb86c3d0e4bcf25b5bf6a
odin thomas rochmann <odin.rochmann@gmail.com>
2021-09-30 11:55:07 +0200

    fix(storybook): set doc as default view mode

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -2,6 +2,10 @@ import '@equinor/fusion-wc-theme';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewMode: 'docs',
+  previewTabs: {
+    canvas: { hidden: true },
+  },
 };
 
 export const decorators = [(Story) => <fwc-theme>{Story()}</fwc-theme>];

fb37e2f4b6575fd3b9219ca121ed8e3dfe1c5f04
odin thomas rochmann <odin.rochmann@gmail.com>
2021-10-05 14:05:47 +0200

    docs(storybook): update stories

--- storybook/stories/Components/Checkbox.tsx
+++
@@ -1,9 +0,0 @@
-import extractProps from './extract-props';
-import { CheckboxElement, CheckboxElementProps } from '@equinor/fusion-wc-checkbox';
-CheckboxElement;
-
-export const Checkbox = ({ ...props }: CheckboxElementProps): JSX.Element => (
-  <fwc-checkbox {...extractProps<CheckboxElementProps>(props)} />
-);
-
-export default Checkbox;

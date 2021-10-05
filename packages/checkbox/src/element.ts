42d1d067f45a44e96e16443996e0de52275e51c4
odin thomas rochmann <odin.rochmann@gmail.com>
2021-10-05 12:59:52 +0200

    feat(checkbox): allow sizing of checkbox
    + add css attrbiute for size
    + add css attribute for padding
    + update storybook

--- packages/checkbox/src/element.ts
+++ packages/checkbox/src/element.ts
@@ -1,18 +1,20 @@
-import { CSSResult } from 'lit';
 import { CheckboxBase } from '@material/mwc-checkbox/mwc-checkbox-base';
-import { styles as mdcStyle } from '@material/mwc-checkbox/mwc-checkbox.css';
-import style from './element.css';
+import styles from './element.css';
 
-export type CheckboxElementProps = {
-  checked?: boolean;
-  disabled?: boolean;
-  indeterminate?: boolean;
-  reducedTouchTarget?: boolean;
-  value?: string;
-};
+/**
+ * @tag fwc-checkbox
+ *
+ * @property {boolean} checked - checked if true
+ * @property {boolean} indeterminate - indicate that not touched
+ * @property {string} name - name of element
+ * @property {boolean} disabled - disable interactivity of element
+ * @property {boolean} reducedTouchTarget - reduce touch size of lement
+ *
+ * @cssprop {18px default, 14px reducedTouchTarget} --fwc-checkbox-size  - size of checkbox. **note** setting size will override `reducedTouchTarget`
+ * @cssprop {--fwc-checkbox-size / 2} --fwc-checkbox-spacing - Number of pixels padding around checkbox
+ */
+export class CheckboxElement extends CheckboxBase {}
 
-export class CheckboxElement extends CheckboxBase implements CheckboxElementProps {
-  static styles: CSSResult[] = [mdcStyle, style];
-}
+CheckboxElement.styles = styles;
 
 export default CheckboxElement;
--- packages/checkbox/src/index.ts
+++ packages/checkbox/src/index.ts
@@ -1,5 +1,5 @@
 import { fusionElement } from '@equinor/fusion-wc-core';
-import CheckboxElement, { CheckboxElementProps } from './element';
+import CheckboxElement from './element';
 export * from './element';
 
 export const tag = 'fwc-checkbox';
@@ -10,14 +10,5 @@ export default class _ extends CheckboxElement {}
 declare global {
   interface HTMLElementTagNameMap {
     [tag]: CheckboxElement;
-  }
-
-  namespace JSX {
-    interface IntrinsicElements {
-      [tag]: React.DetailedHTMLProps<
-        React.PropsWithChildren<CheckboxElementProps & React.HTMLAttributes<CheckboxElement>>,
-        CheckboxElement
-      >;
-    }
   }
 }

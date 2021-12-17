961c07367f8ebda27a2a06531b3a6c72943af24c
Thomas Myklebust Fjørstad <56077030+thomasFjorstad@users.noreply.github.com>
2021-12-17 09:35:17 +0000

    fix(Button): replace old variables in button element css

--- packages/button/src/element.css.ts
+++ packages/button/src/element.css.ts
@@ -12,34 +12,33 @@ export const style = css`
     --mdc-typography-button-text-transform: none;
   }
   :host([variant='contained'][color='primary']) .mdc-button {
-    background-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    --mdc-theme-primary: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
   }
   :host([variant='contained'][color='secondary']) .mdc-button {
-    background-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+    --mdc-theme-primary: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
   }
   :host([variant='contained'][color='danger']) .mdc-button {
-    background-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    --mdc-theme-primary: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
   }
   :host([variant='outlined'][color='primary']) .mdc-button {
-    border-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
-    color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
   }
   :host([variant='outlined'][color='secondary']) .mdc-button {
-    border-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
-    color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
   }
   :host([variant='outlined'][color='danger']) .mdc-button {
-    border-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
-    color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    --mdc-theme-primary: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
   }
   :host([variant='ghost'][color='primary']) .mdc-button {
-    color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.primary__resting.getVariable('color'))};
   }
   :host([variant='ghost'][color='secondary']) .mdc-button {
-    color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.secondary__resting.getVariable('color'))};
   }
   :host([variant='ghost'][color='danger']) .mdc-button {
-    color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    --mdc-button-outline-color: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
+    --mdc-theme-primary: ${unsafeCSS(theme.colors.interactive.danger__resting.getVariable('color'))};
   }
   .leading-icon ::slotted(*),
   .trailing-icon ::slotted(*) {

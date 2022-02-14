c38d9074b7bd1c50cddc88f120dd6666feaad6b0
Silje Stangeland <scst@equinor.com>
2022-02-14 16:24:56 +0100

    feat(circular): Added story component

--- storybook/stories/Components/ProgressIndicator.tsx
+++ storybook/stories/Components/ProgressIndicator.tsx
@@ -1,8 +1,22 @@
 import extractProps from './extract-props';
 
-import { StarProgressElement, StarProgressElementProps } from '@equinor/fusion-wc-progress-indicator';
+import {
+  StarProgressElement,
+  StarProgressElementProps,
+  CircularProgressElement,
+  CircularProgressElementProps,
+} from '@equinor/fusion-wc-progress-indicator';
 StarProgressElement;
 
 export const StarIndicator = ({ children, ...props }: React.PropsWithChildren<StarProgressElement>): JSX.Element => {
   return <fwc-star-progress {...extractProps<StarProgressElementProps>(props)}>{children}</fwc-star-progress>;
+};
+
+export const CircularIndicator = ({
+  children,
+  ...props
+}: React.PropsWithChildren<CircularProgressElement>): JSX.Element => {
+  return (
+    <fwc-circular-progress {...extractProps<CircularProgressElementProps>(props)}>{children}</fwc-circular-progress>
+  );
 };

34eebf2e1b2f0a2379b38037ff594dbf61280ecd
Silje Stangeland <scst@equinor.com>
2022-02-17 13:37:25 +0100

    feat(dots): dots progress indicator component

--- packages/progress-indicator/src/index.ts
+++ packages/progress-indicator/src/index.ts
@@ -1,1 +1,2 @@
 export * from './star';
+export * from './dots';
--- storybook/stories/Components/ProgressIndicator.tsx
+++ storybook/stories/Components/ProgressIndicator.tsx
@@ -1,8 +1,17 @@
 import extractProps from './extract-props';
 
-import { StarProgressElement, StarProgressElementProps } from '@equinor/fusion-wc-progress-indicator';
+import {
+  StarProgressElement,
+  StarProgressElementProps,
+  DotsProgressElement,
+  DotsProgressElementProps,
+} from '@equinor/fusion-wc-progress-indicator';
 StarProgressElement;
 
 export const StarIndicator = ({ children, ...props }: React.PropsWithChildren<StarProgressElement>): JSX.Element => {
   return <fwc-star-progress {...extractProps<StarProgressElementProps>(props)}>{children}</fwc-star-progress>;
+};
+
+export const DotsIndicator = ({ children, ...props }: React.PropsWithChildren<DotsProgressElement>): JSX.Element => {
+  return <fwc-dots-progress {...extractProps<DotsProgressElementProps>(props)}>{children}</fwc-dots-progress>;
 };

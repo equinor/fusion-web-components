3674581200eaa381a66653275b5f8ca3fe64b1ce
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-02-17 10:33:13 +0100

    feat(person): public placeholders

--- packages/person/src/person/element.ts
+++ packages/person/src/person/element.ts
@@ -1,10 +1,11 @@
-import { LitElement } from 'lit';
+import { html, LitElement, TemplateResult } from 'lit';
 import { Task } from '@lit-labs/task';
 import { property } from 'lit/decorators.js';
 import { PersonElementProps } from './types';
 import { PersonPresence, PersonDetails } from '../types';
 import { PersonHost } from '../person-provider';
 import { PersonController } from '../person-provider';
+import { SkeletonSize, SkeletonVariant } from '@equinor/fusion-wc-skeleton';
 
 /**
  * @property {string} azureId - Azure unique id for the person.
@@ -27,6 +28,37 @@ export class PersonElement extends LitElement implements PersonHost, PersonEleme
   constructor() {
     super();
     this.controller = new PersonController(this);
+  }
+
+  /**
+   * Renders pending state for content
+   */
+  public renderTextPlaceholder(inactive?: boolean, size?: SkeletonSize): TemplateResult {
+    return html`<fwc-skeleton
+      size=${size}
+      variant=${SkeletonVariant.Text}
+      icon="image"
+      ?inactive=${inactive}
+    ></fwc-skeleton>`;
+  }
+
+  /**
+   * Renders pending state for avatar
+   */
+  public renderImagePlaceholder(inactive?: boolean, size?: string): TemplateResult {
+    return html`<fwc-skeleton
+      size=${size}
+      variant=${SkeletonVariant.Circle}
+      icon="image"
+      ?inactive=${inactive}
+    ></fwc-skeleton>`;
+  }
+
+  /**
+   * Returns the first character in the person's name as upper case initial
+   */
+  public getInitial(name?: string): string | undefined {
+    return name?.substring(0, 1)?.toUpperCase();
   }
 }
 

32ee91591e3bb10b1bbbbe21ff9970867d56b30d
Asbjørn Håland <github@asbjornhaland.no>
2023-05-09 16:05:09 +0200

    feat(markdown): add markdown-viewer
    Closes #592

--- packages/markdown/src/index.ts
+++ packages/markdown/src/index.ts
@@ -1,2 +1,13 @@
-export { default as MarkdownEditorElement, MarkdownEditorElementProps, tag } from './markdown-editor';
+export {
+  default as MarkdownEditorElement,
+  MarkdownEditorElementProps,
+  tag as markdownEditorTag,
+} from './markdown-editor';
+
 export { MenuSizes } from './types';
+
+export {
+  default as MarkdownViewerElement,
+  MarkdownViewerElementProps,
+  tag as markdownViewerTag,
+} from './markdown-viewer';

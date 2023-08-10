000f2b81f37dcb1fe545ecdd3c1e26d9d57289f3
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-08-10 13:00:59 +0200

    fix(person): account type background

--- packages/person/src/person-avatar/element.css.ts
+++ packages/person/src/person-avatar/element.css.ts
@@ -2,19 +2,19 @@ import { css } from 'lit';
 
 export const style = css`
   :host(:not([disabled])) .fwc-person-avatar__employee {
-    --fwc-avatar-base-color: #771fdd;
+    --fwc-avatar-base-color: #8c1159;
+    --fwc-avatar-ink-color: #fff;
+  }
+  :host(:not([disabled])) .fwc-person-avatar__consultant {
+    --fwc-avatar-base-color: #eb0037;
+    --fwc-avatar-ink-color: #fff;
+  }
+  :host(:not([disabled])) .fwc-person-avatar__external {
+    --fwc-avatar-base-color: #ff92a8;
     --fwc-avatar-ink-color: #fff;
   }
   :host(:not([disabled])) .fwc-person-avatar__external-hire {
-    --fwc-avatar-base-color: #243746;
-    --fwc-avatar-ink-color: #fff;
-  }
-  :host(:not([disabled])) .fwc-person-avatar__x-external {
-    --fwc-avatar-base-color: #ff1243;
-    --fwc-avatar-ink-color: #fff;
-  }
-  :host(:not([disabled])) .fwc-person-avatar__joint-venture-affiliate {
-    --fwc-avatar-base-color: #ff93ee;
+    --fwc-avatar-base-color: #000;
     --fwc-avatar-ink-color: #fff;
   }
 `;

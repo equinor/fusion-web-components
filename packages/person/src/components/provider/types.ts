aafff0bbcda007749cfbee38d6d79655754a9665
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-09-16 12:43:35 +0200

    fix(person): update resolver arg and response types

--- packages/person/src/components/provider/types.ts
+++ packages/person/src/components/provider/types.ts
@@ -1,14 +1,20 @@
 import { ReactiveControllerHost } from 'lit';
 import { PersonDetails, AzureIdOrUpnObj, PersonInfo, PersonSearchResult } from '../../types';
 
+type ResolverArgs<T = unknown> = T extends object
+  ? { [K in keyof T]: T[K] } & { signal?: AbortSignal }
+  : { signal?: AbortSignal };
+
+type ResolverResult<T = unknown> = T | Promise<T>;
+
 export interface PersonHost extends ReactiveControllerHost {
   azureId: string;
   dispatchEvent(event: Event): boolean;
 }
 
 export interface PersonResolver {
-  getDetails: (args: AzureIdOrUpnObj) => Promise<PersonDetails> | PersonDetails;
-  getInfo: (args: AzureIdOrUpnObj) => Promise<PersonInfo> | PersonInfo;
-  getPhoto: (args: AzureIdOrUpnObj) => Promise<string> | string;
-  search: (args: { search: string }) => Promise<PersonSearchResult> | PersonSearchResult;
+  getDetails: (args: ResolverArgs<AzureIdOrUpnObj>) => ResolverResult<PersonDetails>;
+  getInfo: (args: ResolverArgs<AzureIdOrUpnObj>) => ResolverResult<PersonInfo>;
+  getPhoto: (args: ResolverArgs<AzureIdOrUpnObj>) => ResolverResult<string>;
+  search: (args: ResolverArgs<{ search: string }>) => ResolverResult<PersonSearchResult>;
 }

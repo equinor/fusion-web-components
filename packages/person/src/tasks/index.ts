15fbfbc1274e64ced8114bebb96931091abc9f6a
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-09-20 17:00:06 +0200

    fix(person): remove unused export

--- packages/person/src/tasks/index.ts
+++ packages/person/src/tasks/index.ts
@@ -3,5 +3,3 @@ export { PersonDetailTask } from './person-detail-task';
 export { PersonInfoTask, PersonInfoControllerHost } from './person-info-task';
 export { PersonPhotoTask } from './person-photo-task';
 export { PersonSearchTask } from './person-search-task';
-
-export { TaskError, type TaskErrorType } from './errors';

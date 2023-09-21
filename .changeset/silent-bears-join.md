fb8434b56f23ab98b546cb88ded76d464fb67549
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-09-21 15:44:17 +0200

    fix(person): circular refs (#856)

---
+++ .changeset/silent-bears-join.md
@@ -0,0 +1,5 @@
+---
+'@equinor/fusion-wc-person': patch
+---
+
+expose upn in PersonInfo interface
---
+++ packages/person/src/components/select/index.ts
@@ -0,0 +1,26 @@
+import { fusionElement } from '@equinor/fusion-wc-core';
+import PersonSelectElement from './element';
+import { type SearchableDropdownProps } from '@equinor/fusion-wc-searchable-dropdown';
+
+export * from './element';
+
+export type PersonSelectElementProps = Partial<SearchableDropdownProps>;
+
+export const tag = 'fwc-person-select';
+
+@fusionElement(tag)
+export default class _ extends PersonSelectElement {}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [tag]: PersonSelectElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      [tag]: React.DetailedHTMLProps<
+        React.PropsWithChildren<PersonSelectElementProps & React.HTMLAttributes<PersonSelectElement>>,
+        PersonSelectElement
+      >;
+    }
+  }
+}

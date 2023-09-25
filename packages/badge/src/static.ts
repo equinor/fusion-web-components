46022e645ef8bd2bda4734227fd5e395854891c2
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-09-26 01:35:41 +0200

    feat(badge): update badge stories

---
+++ packages/badge/src/static.ts
@@ -0,0 +1,22 @@
+export const BadgeSize = {
+  XSmall: 'x-small',
+  Small: 'small',
+  Medium: 'medium',
+  Large: 'large',
+} as const;
+
+export const BadgePosition = {
+  TopLeft: 'top-left',
+  TopRight: 'top-right',
+  BottomLeft: 'bottom-left',
+  BottomRight: 'bottom-right',
+} as const;
+
+export const BadgeColor = {
+  Primary: 'primary',
+  Secondary: 'secondary',
+  Success: 'success',
+  Danger: 'danger',
+  Warning: 'warning',
+  Disabled: 'disabled',
+} as const;

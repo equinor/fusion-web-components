d1e996da997b2abf36e6a9203ff92eb39de62966
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-09-29 11:09:47 +0200

    feat: add general component for storybook

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -1,19 +1,34 @@
-// import {type StorybookConfig} from '@storybook/web-components-vite';
 import {type StorybookConfig} from '@storybook/web-components-vite';
+import { mergeConfig } from 'vite';
+import { resolve } from 'path'
 
 export const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.ts', 
+    {
+      directory: '../stories/data-display',
+      titlePrefix: 'Data Display'
+    },
+    {
+      directory: '../stories/input',
+      titlePrefix: 'Input'
+    }
   ],
   framework: '@storybook/web-components-vite',
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-docs'
   ],
-  docs: {
-    autodocs: 'tag',
-  }
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      resolve: {
+        alias: [
+            { find: '@components', replacement: resolve(__dirname, '../components') },
+        ],
+    },
+    });
+  },
 };
 
 export default config;

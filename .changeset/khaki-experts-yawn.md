08dbb0e47678317219c8b784ba648e4c9af89664
Asbjørn Håland <github@asbjornhaland.no>
2023-10-23 10:33:49 +0200

    feat(storybook) add storybook and stories for markdown-viewer

---
+++ .changeset/khaki-experts-yawn.md
@@ -0,0 +1,6 @@
+---
+'@equinor/fusion-wc-markdown': minor
+'@equinor/fusion-wc-storybook': minor
+---
+
+Add storybook and stories for markdown-viewer
---
+++ storybook/stories/input/markdown-viewer.stories.ts
@@ -0,0 +1,32 @@
+import { html } from 'lit';
+
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { setCustomElementsManifest } from '@storybook/web-components';
+
+import {
+  MarkdownViewerElement,
+  MarkdownViewerElementProps,
+} from '@equinor/fusion-wc-markdown';
+import cem from '@equinor/fusion-wc-markdown/custom-elements.json';
+import markdownExample from './markdown.example.md?raw';
+
+MarkdownViewerElement;
+
+setCustomElementsManifest(cem);
+
+type Story = StoryObj<MarkdownViewerElementProps>;
+
+const meta: Meta<typeof cem> = {
+  component: 'fwc-markdown-viewer',
+};
+
+const render = (_props: MarkdownViewerElementProps) => html`
+  <fwc-markdown-viewer>${markdownExample}</fwc-markdown-viewer>
+`;
+
+export const Default: Story = {
+  args: {},
+  render,
+};
+
+export default meta;

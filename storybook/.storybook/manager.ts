f1326afc147162f6bc1069b180277ab99122017c
Øyvind Eikeland <oyvind@eikeland.me>
2023-11-10 10:10:01 +0100

    docs(storybook): adding dark theme to storybook

--- storybook/.storybook/manager.ts
+++ storybook/.storybook/manager.ts
@@ -1,12 +1,12 @@
 import { addons } from '@storybook/manager-api';
-import { create } from '@storybook/theming/create';
+import { create } from '@storybook/theming';
 
-const theme = create({
-  base: 'light',
+export const theme = create({
+  base: 'dark',
   brandTitle: 'Fusion Web Components',
   brandUrl: 'https://github.com/equinor/fusion-web-components',
-  colorPrimary: '#FF1243',
-  colorSecondary: '#007079',
+  colorPrimary: '#007079',
+  colorSecondary: '#FF1243',
   fontBase: 'Equinor, sans-serif',
   fontCode: '"Operator Mono","Fira Code Retina","Fira Code","FiraCode-Retina","Andale Mono","Lucida Console",Consolas,Monaco,monospace'
 });

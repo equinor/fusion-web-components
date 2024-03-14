817f238a1a8906d85f6491285ac7ef736d048ac8
Øyvind Eikeland <oyvind@eikeland.me>
2024-03-14 13:34:14 +0100

    fix(storybook-person): fix wrong ending tag for personselect story

--- storybook/stories/person/person-select.stories.ts
+++ storybook/stories/person/person-select.stories.ts
@@ -26,30 +26,30 @@ const meta: Meta<typeof cem> = {
 const render = (props: PersonSelectElementProps) => {
   const selectedPerson = props.selectedPerson === null ? '' : props.selectedPerson;
   return html`
-      <div style="min-height: 300px">
-        <fwc-person-select
-          autofocus="${ifDefined(props.autofocus)}"
-          selectedPerson=${ifDefined(selectedPerson)}
-          selectTextOnFocus=${ifDefined(props.selectTextOnFocus)}
-          disabled=${ifDefined(props.disabled)}
-          dropdownHeight=${ifDefined(props.dropdownHeight)}
-          graphic=${ifDefined(props.graphic)}
-          initialText=${ifDefined(props.initialText)}
-          label=${ifDefined(props.label)}
-          leadingIcon=${ifDefined(props.leadingIcon)}
-          meta=${ifDefined(props.meta)}
-          multiple=${ifDefined(props.multiple)}
-          placeholder=${ifDefined(props.placeholder)}
-          selectedId=${ifDefined(props.selectedId)}
-          textInputElement=${ifDefined(props.textInputElement)}
-          listElement=${ifDefined(props.listElement)}
-          value=${ifDefined(props.value)}
-          variant=${ifDefined(props.variant)}
-          @select=${(e: PersonSelectEvent) => {
-            console.log('SelectEvent', e);
-          }}
-        ></fwc-person-avatar>
-      </div>
+    <div style="min-height: 300px">
+      <fwc-person-select
+        autofocus="${ifDefined(props.autofocus)}"
+        selectedPerson=${ifDefined(selectedPerson)}
+        selectTextOnFocus=${ifDefined(props.selectTextOnFocus)}
+        disabled=${ifDefined(props.disabled)}
+        dropdownHeight=${ifDefined(props.dropdownHeight)}
+        graphic=${ifDefined(props.graphic)}
+        initialText=${ifDefined(props.initialText)}
+        label=${ifDefined(props.label)}
+        leadingIcon=${ifDefined(props.leadingIcon)}
+        meta=${ifDefined(props.meta)}
+        multiple=${ifDefined(props.multiple)}
+        placeholder=${ifDefined(props.placeholder)}
+        selectedId=${ifDefined(props.selectedId)}
+        textInputElement=${ifDefined(props.textInputElement)}
+        listElement=${ifDefined(props.listElement)}
+        value=${ifDefined(props.value)}
+        variant=${ifDefined(props.variant)}
+        @select=${(e: PersonSelectEvent) => {
+          console.log('SelectEvent', e);
+        }}
+      ></fwc-person-select>
+    </div>
   `;
 };
 
@@ -88,6 +88,5 @@ export const ClearSelectedPersonOnSelect: Story = {
   },
   render,
 };
-
 
 export default meta;

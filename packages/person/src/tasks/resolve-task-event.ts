801d9b77c8e3422e5aa1b49b288f3a204a475bdd
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-12-05 09:22:21 +0100

    fix(resolve-task-event): streamline event resolution by removing redundant frame wait

--- packages/person/src/tasks/resolve-task-event.ts
+++ packages/person/src/tasks/resolve-task-event.ts
@@ -2,6 +2,11 @@ export const resolveTaskEvent = async <TData>(
   host: EventTarget,
   event: CustomEvent<{ result?: TData | Promise<TData> }>,
 ): Promise<TData> => {
+  // Wait for the next frame to allow providers to connect and setup
+  await new Promise<void>((res): void => {
+    window.requestAnimationFrame(() => res());
+  });
+
   // Create a promise that will resolve when the event is resolved
   const job = new Promise<TData>((res, rej): void => {
     // Dispatch the event
@@ -19,11 +24,6 @@ export const resolveTaskEvent = async <TData>(
 
     // If no resolver has committed to resolving the request, reject
     rej(new Error('No resolver resolved the request'));
-  });
-
-  // Wait for the next frame to allow providers to connect and setup
-  await new Promise<void>((res): void => {
-    window.requestAnimationFrame(() => res());
   });
 
   // Wait for the result of the request

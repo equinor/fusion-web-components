b7597e78f91a19e101ad166d4563d2f1adce95b2
Asbjørn Håland <github@asbjornhaland.no>
2026-01-05 13:52:36 +0100

    feat(deps): update storybook to 10.1.11

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -1,7 +1,12 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { fileURLToPath } from "node:url";
 import remarkGfm from 'remark-gfm';
 import {type StorybookConfig} from '@storybook/web-components-vite';
 import { mergeConfig } from 'vite';
-import { resolve } from 'path'
+import { resolve, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const config: StorybookConfig = {
   stories: [
